d872b741c2a27a29d20b5bd4c01d07771ce810d6
HP <HP@DESKTOP-L89B3LR>
2021-02-25 12:00:19 +0300

    Car

---
+++ Car/src/Car2.java
@@ -0,0 +1,37 @@
+
+public class Car2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  String color;
+  int year;
+  String model;
+  String company;
+  double price;
+  
+  
+  Car2(){
+	  
+  }
+	  
+	}
+  Car2(newColor: String , newYear: int , newModel: String , newCompany: String ,  newprice: double){
+	  
+	  
+	  
+  color=newColor;
+  year=newYear;
+  model=newModel;
+  company=newCompany;
+  price=newPrice;
+  }
+String getCompany() {
+return company;	
+}
+double getPrice();{
+	return price;
+}
+
+
+}
+
